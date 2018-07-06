b197844f27d15aed31a917017cf6e8396ebd0c4e
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2018-07-06 17:36:05 +0300

    requests

--- src/admin/requests.js
+++ src/admin/requests.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import store from "./store";
 
 axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.headers["Authorization"] = `Bearer ${localStorage.getItem(
@@ -15,33 +14,17 @@ axios.interceptors.response.use(
     const ttl = parseInt(localStorage.getItem("ttl"));
     const DateNow = Math.floor(Date.now() / 1000);
     const originalRequest = error.config;
-    console.log("токен ошибка", DateNow, ttl, DateNow + 10 > ttl);
-    console.log("originalRequest", error.config);
-    console.log("cancell", axios.isCancel());
     if (DateNow > ttl && error.response.status === 401) {
-      console.log("токен протух");
-      axios
-        .post("/refreshToken")
-        .then(
-          response => {
-            console.log("ответ при рефреше", response);
-            const token = response.data.token;
-            const ttl = Math.floor(Date.now() / 1000 + response.data.ttl);
-            localStorage.setItem("token", token);
-            localStorage.setItem("ttl", ttl);
-            axios.defaults.headers["Authorization"] = `Bearer ${token}`;
-            originalRequest.headers["Authorization"] = `Bearer ${token}`;
+      axios.post("/refreshToken").then(response => {
+        const token = response.data.token;
+        const ttl = Math.floor(Date.now() / 1000 + response.data.ttl);
+        localStorage.setItem("token", token);
+        localStorage.setItem("ttl", ttl);
+        axios.defaults.headers["Authorization"] = `Bearer ${token}`;
+        originalRequest.headers["Authorization"] = `Bearer ${token}`;
 
-            // store.dispatch("getUserInfo");
-            return axios(originalRequest);
-          },
-          error => {
-            console.log("propm", error);
-          }
-        )
-        .catch(e => {
-          console.log("errorro", e);
-        });
+        return axios(originalRequest);
+      });
     }
 
     return Promise.reject(error);
